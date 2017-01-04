6aa0a86be2612a084f7f1f237671e0cfa3474f24
RafaelCalandreli <rafaelc_jp_21@hotmail.com>
2017-01-03 21:04:50 -0300

    first commit

---
+++ app/src/main/java/abbraconsultoria/com/br/isignos/MainActivity.java
@@ -0,0 +1,13 @@
+package abbraconsultoria.com.br.isignos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
